913f3dafe9e09f2b35a29b2c49c904ba40ddcb96
enthu <enthudave@gmail.com>
2016-09-29 21:09:48 +0200

    update utils/pulldown.sh

--- utils/pulldown.sh
+++ utils/pulldown.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+
+sudo apt install ssh tmux zsh git vim neovim
+sudo apt install software-properties-common python-pip python-dev python3-pip python3-dev
+sudo pip install neovim
+
 cp ./zsh/zshrc ~/.zshrc ./zsh/zshenv ~/.zshenv ./tmux/tmux.config ~/.tmux.config
-cp ./nvim/vimrc ~/.vimrc
+cp ./nvim/vimrc ~/.vimrc ./tmux/tmux.conf ~/.tmux.conf
+
+mkdir ~/.config/nvim
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
