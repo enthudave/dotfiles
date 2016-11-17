2f213926d6287dfbc0da46ad63366fa16097bdfa
enthudave <enthudave@gmail.com>
2016-11-17 20:00:04 +0100

    make executable

--- linux/pulldown-debian.sh
+++ linux/pulldown-debian.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-printf "${RED}APT SECTION${NC}\n\n\n"
+printf "${RED}APT SECTION${NC}\n\n"
 
 # sudo apt install ssh tmux
 sudo apt install zsh git curl terminator
@@ -11,9 +11,9 @@ sudo apt install python-pip python-dev python3-pip python3-dev
 sudo apt install libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 sudo apt install python-gi python-gi-cairo python-cairo
 
-printf "${RED}INSTALL NEOVIM${NC}\n\n\n"
+printf "\n\n${RED}INSTALL NEOVIM${NC}\n\n"
 
-if [[ ! -x "/usr/local/bin/nvim" ]];
+if [ ! -x "/usr/local/bin/nvim" ];
 then
 	git clone https://github.com/neovim/neovim ~/Desktop/neovim
 	cd ~/Desktop/neovim
@@ -21,38 +21,53 @@ then
 	sudo make install
 fi
 
-printf "${RED}INSTALL NEOVIM GUI AND PYTHON API${NC}\n\n\n"
+printf "\n\n${RED}INSTALL NEOVIM GUI AND PYTHON API${NC}\n\n"
 
 sudo pip install neovim
 sudo pip install click
 sudo pip install neovim-gui
 
-printf "${RED}INSTALL OH-MY-ZSH${NC}\n\n\n"
+printf "\n\n${RED}INSTALL OH-MY-ZSH${NC}\n\n"
 
-if [[ ! -d "$(dirname ~/.oh-my-zsh)" ]];then
+if [ ! -d "$HOME/.oh-my-zsh" ];then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-printf "${RED}INSTALL OH-MY-ZSH THEME${NC}\n\n\n"
+printf "\n\n${RED}INSTALL OH-MY-ZSH THEME${NC}\n\n"
 
-if [[ ! -d "$(dirname ~/.oh-my-zsh/custom/themes/powerlevel9k)" ]];then
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k" ];then
 	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
 
-printf "${RED}INSTALL DOTFILES${NC}\n\n\n"
+printf "\n\n${RED}INSTALL DOTFILES${NC}\n\n"
 
-if [[ ! -d "/home/enthu/Desktop/dotfiles)" ]];then
+if [ ! -d "$HOME/Desktop/dotfiles)" ];then
 	git clone https://github.com/enthudave/dotfiles ~/Desktop/dotfiles
-	ln -s ~/Desktop/dotfiles/linux/zsh/zshrc ~/.zshrc
-	ln -s ~/Desktop/dotfiles/linux/zsh/zshenv ~/.zshenv
-	mkdir ~/.config/
-	ln -s ~/.vim ~/.config/nvim
-	ln -s ~/Desktop/dotfiles/linux/nvim/vimrc ~/.config/nvim/init.vim
+	if [ ! -e "$HOME/.zshrc" ];then
+		rm $HOME/.zshrc
+		ln -s ~/Desktop/dotfiles/linux/zsh/zshrc ~/.zshrc
+	fi
+	if [ ! -e "$HOME/.zshenv" ];then
+		rm $HOME/.zshenv
+		ln -s ~/Desktop/dotfiles/linux/zsh/zshenv ~/.zshenv
+	fi
+	if [ ! -d "$HOME/.config" ];then
+		mkdir ~/.config/
+	fi
+	if [ ! -e "$HOME/.config/nvim" ];then
+		if [ ! -d "$HOME/.vim" ];then
+			mkdir $HOME/.vim
+		fi
+		ln -s ~/.vim ~/.config/nvim
+	fi
+	if [ ! -e "$HOME/.config/nvim/init.vim" ];then
+		ln -s ~/Desktop/dotfiles/linux/nvim/vimrc ~/.config/nvim/init.vim
+	fi
 fi
 
-printf "${RED}INSTALL FONTS${NC}\n\n\n"
+printf "\n\n${RED}INSTALL FONTS${NC}\n\n"
 
-if [[ ! -d "$(dirname ~/.local/share/fonts/NerdFonts)" ]];then
+if [ ! -d "$HOME/.local/share/fonts/NerdFonts" ];then
 	git clone https://github.com/ryanoasis/nerd-fonts ~/Desktop/nerd-fonts
 	cd ~/Desktop/nerd-fonts
 	./install.sh SourceCodePro
@@ -60,8 +75,7 @@ if [[ ! -d "$(dirname ~/.local/share/fonts/NerdFonts)" ]];then
 	#rm -rf ~/Desktop/nerd-fonts
 fi
 
-if [ ! -d "$(dirname ~/Desktop/powerline-fonts)" ];
-then
+if [ ! -d "$HOME/Desktop/powerline-fonts" ];then
 	git clone https://github.com/powerline/fonts ~/Desktop/powerline-fonts
 	~/Desktop/powerline-fonts/install.sh
 fi
